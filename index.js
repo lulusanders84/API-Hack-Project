e708aede42f7b3792049952cace1bee76e98f421
Lucy Sanders <lucyhsanders@gmail.com>
2018-06-26 10:09:40 -0500

    football data request and roster array build working

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+
 const FIFA_FIXTURES_URL = "https://fifa-2018-apis.herokuapp.com/fifa/fixtures";
 const FOOTBALL_DATA_TEAMS_URL = 'https://api.football-data.org/v1/competitions/467/teams';
 const WIKIPEDIA_SEARCH_URL = "https://en.wikipedia.org/w/api.php";
 let groupStageFixtures = [];
+let roster = [];
 const teams = {
 	groupA: ["Russia", "Saudi Arabia", "Egypt", "Uruguay"],
 	groupB: ["Portugal", "Spain", "Morocco", "Iran"],
@@ -92,6 +94,61 @@ const teams = {
 		$('#countries').html(options);
 	}
 
+//functions that retrieve roster from football data and build roster array
+	function getRosterArray(country) {
+		console.log("getRosterArray ran");
+		getFootballDataApiData(FOOTBALL_DATA_TEAMS_URL, callbackFootballDataApiData, country)
+	}
+
+	function getFootballDataApiData(apiUrl, callbackFunc, country) {
+		console.log("getFootballDataApiData ran with apiUrl:", apiUrl);
+		$.ajax({
+  			headers: { 'X-Auth-Token': 'f7302355bfde4075a668246ec2d7056e' },
+  			url: apiUrl,
+  			dataType: 'json',
+  			type: 'GET',
+		}).done(function(response) {
+			console.log("getFootballDataApiData request is done")
+ 			callbackFunc(response, country);
+		});
+	}
+
+	function callbackFootballDataApiData(response, country) {
+		console.log("callbackFootballDataApiData ran");
+		const teams = response.teams;
+  		const FOOTBALL_DATA_PLAYERS_URL = teams.reduce((acc, team) => {
+  			if (team.name === country) {
+  				const href = team._links.players.href;
+  				acc = href.substring(0, 4) + "s" + href.substring(4);
+  			}
+  			return acc;
+  		}, "");
+  		getFootballDataApiData(FOOTBALL_DATA_PLAYERS_URL, callbackFootballDataApiPlayerData);
+	}
+
+	function callbackFootballDataApiPlayerData(response){
+		roster = buildRosterArray(response);
+		console.log(roster);
+	}
+
+	function buildRosterArray(response) {
+		const players = response.players;
+		return players.sort(function(a, b) {
+			if (a.jerseyNumber < b.jerseyNumber) {
+    			return -1;
+  			} else if (a.jerseyNumber > b.jerseyNumber) {
+    			return 1;
+  			}
+  			return 0;
+		});
+	}
+
+//function that handles submit country event
+	function handleCountrySelection(event) {
+		event.preventDefault();
+		const country = $('#countries option:selected').val();
+		getRosterArray(country);
+	}
 function startWorldCupApp() {
 	buildGroupStageFixturesObj();
 	renderSelectCountryOptions();
